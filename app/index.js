b4c83354ae809cbe2143476c5443927e91810198
Ivan <ivan.hf.leung@gmail.com>
2023-08-31 12:52:35 -0400

    updated

--- app/index.js
+++ app/index.js
@@ -1,62 +1,66 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
-import PropTypes from "prop-types";
-//Validating component properties
-/*const MyComponent = function ({
-  myString,
-  myNumber,
-  myBool,
-  myFunc,
-  myArray,
-  myObject,
-}) {
-  return (
-    <View>
-      <Text>{myString}</Text>
-      <Text>{myNumber}</Text>
-      <TextInput editable={myBool} placeholder="Enter text" />
-      <Text>{myFunc()}</Text>
-      <Text>
-        {myArray.map((i) => (
-          <Text key={i}>{i}</Text>
-        ))}
-      </Text>
-      <Text>{myObject.myProp}</Text>
-    </View>
-  );
-};
+import React, { Component } from "react";
+import { View, Text, Button } from "react-native";
 
-MyComponent.propTypes = {
-  myString: PropTypes.string.isRequired,
-  //myNumber: PropTypes.number.isRequired,
-  myNumber: (props, name, component) =>
-    Number.isFinite(props[name]) && props[name] >= 10 && props[name] <= 99
-      ? null
-      : new Error(`${component}.${name} expects a number between 10 and 99`),
-  myBool: PropTypes.bool.isRequired,
-  myFunc: PropTypes.func.isRequired,
-  myArray: PropTypes.array.isRequired,
-  myObject: PropTypes.object.isRequired,
-};
-*/
+class PlayerList extends Component {
+  // Initially, the user has not picked their favourite player.
+  state = {
+    favouritePlayer: null,
+    favouritePlayerNumber: null,
+  };
+
+  // Event handler for the button onPress event.
+  // We only define a single function, it will handle the onPress
+  // event for all of the buttons. It accepts an argument.
+  onPlayerSelected(number) {
+    // Find the object associated to the number of the player.
+    const { name } = this.props.players.find((i) => i.number === number);
+
+    this.setState({ favouritePlayer: name });
+    this.setState({ favouritePlayerNumber: number });
+  }
+
+  render() {
+    const { favouritePlayer } = this.state;
+    const { favouritePlayerNumber } = this.state;
+
+    // Conditionally render the <Text> element.
+    let text;
+
+    if (favouritePlayer)
+      text = (
+        <Text>
+          Your favourite player is {favouritePlayer} and the number is{" "}
+          {favouritePlayerNumber}
+        </Text>
+      );
+    else text = <Text>Choose your favourite player ...</Text>;
+
+    return (
+      <View>
+        {text}
+        {this.props.players.map(({ number, name }) => (
+          // We must bind the event handler context in order for the function to access the
+          // component, specifically the setState() function. In addition, pass the player.number
+          // as an argument to the handler.
+          <Button
+            key={number}
+            title={name}
+            onPress={this.onPlayerSelected.bind(this, number)}
+          />
+        ))}
+      </View>
+    );
+  }
+}
+
 export default function App() {
-  //Event Handling
-  const [status, setStatus] = useState("Waiting");
-  return (
-    //Validating component properties
-    /* <MyComponent
-      myString="String"
-      myNumber={42}
-      myBool={"false"}
-      myFunc={() => "Function"}
-      myArray={["A", "B", "C"]}
-      myObject={{ myProp: "Object.myProp" }}
-    />
-    */
-    //Event Handling
-    <View>
-      <Text>Status: {status}</Text>
-      <Button title="Click Me" onPress={(e) => setStatus("Clicked")} />
-    </View>
-  );
+  // An array of objects passed to the <PlayerList> component as a
+  // prop called players. Each player object will render its own <Button>
+  const players = [
+    { number: 34, name: "Auston Matthews" },
+    { number: 91, name: "John Tavares" },
+    { number: 16, name: "Mitch Marner" },
+  ];
+
+  return <PlayerList players={players} />;
 }
